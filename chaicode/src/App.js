842bdeec9f3159b11fe4b95f750ef3021995a5f1
AbenaLettu97 <eunicemaabenayayra@gmail.com>
2024-12-11 13:03:23 +0000

    commit recent codes

---
+++ chaicode/src/App.js
@@ -0,0 +1,14 @@
+import Youtube from "./Youtube";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>classic react App</h1>
+      <Youtube />
+    </div>
+  );
+}
+
+export default App;
---
+++ chaicode01/src/App.jsx
@@ -0,0 +1,14 @@
+import Youtube from "../../chaicode/src/Youtube";
+
+function App() {
+
+  return (
+    <div>
+   <h1>Vite react App</h1>
+   < Youtube />
+    </div>
+ 
+  )
+}
+
+export default App
