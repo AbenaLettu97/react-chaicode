553bfd7e90c04933b00ac4750fb5bbdcb171c0fa
AbenaLettu97 <eunicemaabenayayra@gmail.com>
2024-12-15 16:15:05 +0000

    react router

---
+++ 07reactrouter/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <h1 className='bg-blue-600 text-white w-full mt-3'>Test for tailwind</h1>
+    </>
+  )
+}
+
+export default App
